f245133f55dd7724b446635289f7c07a356be191
Srujan-q123 <87603868+Srujan-q123@users.noreply.github.com>
2021-08-24 18:17:59 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -1601,14 +1601,11 @@ async def cb_about(bot, update: CallbackQuery):
     text=f"<i><u>Bot's Status</u></i>\n"
     text+=f"\n<b><i>Bot's Uptime:</i></b> <code>{time_formatter(time.time() - start_uptime)}</code>\n"
     text+=f"\n<b><i>Bot Funtion:</i></b> <i>Auto Filter Files</i>\n"
-    text+=f"""\n<b><i>Bot Support:</i></b> <a href="https://t.me/EE_Movies">@EE_MOVIES</a>\n"""
-    text+="""\n<b><i>Source Code:</i></b> <a href="https://t.me/source_code_of_file_store_bot/2">Source</a>"""
-
     buttons = [
         [
             InlineKeyboardButton
                 (
-                    "My Dev ⚡", url="https://t.me/EE_Movies"
+                    "My Dev ⚡", url="https://t.me/Sruja_12"
                 ),
                 
             InlineKeyboardButton
@@ -1640,11 +1637,6 @@ async def callback_data(bot, update: CallbackQuery):
     if query_data == "start":
         buttons = [[
             InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/EE_Movies'),
-            InlineKeyboardButton('Source Code 🧾', url ='https://t.me/source_code_of_file_store_bot/2')
-        ],[
-            InlineKeyboardButton('Support 🛠', url='https://t.me/EE_Movies')
-        ],[
-            InlineKeyboardButton('Help ⚙', callback_data="help")
         ]]
     
         reply_markup = InlineKeyboardMarkup(buttons)
